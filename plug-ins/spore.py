fe64b7949dd68139ed6cbf3dd5678e2b188ce490
anno <anno_schachner@gmx.at>
2018-05-11 14:56:04 +0200

    add spore menu

--- plug-ins/spore.py
+++ plug-ins/spore.py
@@ -1,42 +1,36 @@
 import sys
 
-import pymel
-from maya import mel
 import pymel.core as pm
 import maya.OpenMaya as om
-import maya.OpenMayaUI as omui
 import maya.OpenMayaMPx as ompx
-import maya.OpenMayaRender as omr
-
-from scripted import spore_node
-from scripted import spore_context
-reload(spore_node)
-reload(spore_context)
-
 
 import AEsporeNodeTemplate
+from scripted import spore_node
+from scripted import spore_context
+from scripted import spore_command
+
+reload(spore_node)
+reload(spore_context)
+reload(spore_command)
 reload (AEsporeNodeTemplate)
-from maya import mel
+
+import maya.mel as mel
 mel.eval('refreshEditorTemplates;')
-#  try
-#      import AEsporeNodeTemplate
-#      reload (AEsporeNodeTemplate)
-#      from maya import mel
-#      mel.eval('refreshEditorTemplates;')
-#  except:
-#      raise ImportWarning('Could not import sporeNode Attribute Editor ui')
+
+
+MENU = None
+# add items   #Label                    #commande
+MENU_ITEMS = {'Spore':                  'import manager;reload(manager)',
+              'Create Spore Node':      '',
+              'Help':                   ''}
+
 
 def initializePlugin(mobject):
+    """ initialize plugins & create menu"""
 
     mplugin = ompx.MFnPlugin(mobject)
 
-    # TODO add menu!
-    #  mplugin.addMenuItem("Instance Along Curve", "MayaWindow|mainEditMenu", kPluginCmdName, "")
-
-    pm.callbacks(addCallback=load_spore_template,
-                 hook='provideAETemplateForNodeType',
-                 owner=spore_node.SporeNode.name)
-
+    # register node prototype
     try:
         mplugin.registerNode(spore_node.SporeNode.name,
                              spore_node.SporeNode.id,
@@ -48,7 +42,7 @@ def initializePlugin(mobject):
         sys.stderr.write( "Failed to register node: %s" % spore_node.SporeNode.name)
         raise
 
-
+    # register context & tool command
     try:
         mplugin.registerContextCommand(spore_context.K_CONTEXT_NAME,
                                        spore_context.SporeContextCommand.creator,
@@ -56,17 +50,32 @@ def initializePlugin(mobject):
                                        spore_context.SporeToolCmd.creator,
                                        spore_context.SporeToolCmd.syntax)
     except:
-        sys.stderr.write("Failed to register context command: %s\n" % spore_context.K_CONTEXT_NAME)
+        sys.stderr.write("Failed to register context command: {}".format(spore_context.K_CONTEXT_NAME))
         raise
 
+    try:
+        mplugin.registerCommand(spore_command.SporeCommand.name,
+                                spore_command.SporeCommand.creator,
+                                spore_command.SporeCommand.syntax)
+    except:
+        sys.stderr.write('Failed to register spore command: {}'.format(spore_command.SporeCommand.name))
+
+    # cereate menu
+    global MENU
+    if not MENU:
+        main_wnd = pm.language.melGlobals['gMainWindow']
+        MENU = pm.menu('Spore', parent=main_wnd)
+
+    # add menu items
+    for lbl, cmd in MENU_ITEMS.iteritems():
+        pm.menuItem(label=lbl, command=cmd, parent=MENU)
+
 def uninitializePlugin(mobject):
+    """ uninitialize plugins in reverse order & delete menu """
 
     mplugin = ompx.MFnPlugin(mobject)
 
-    pm.callbacks(removeCallback=load_spore_template,
-                 hook='AETemplateCustomContent',
-                 owner=spore_node.SporeNode.name)
-
+    # deregister context and tool command
     try:
         mplugin.deregisterContextCommand(spore_context.K_CONTEXT_NAME,
                                          spore_context.K_TOOL_CMD_NAME)
@@ -74,23 +83,27 @@ def uninitializePlugin(mobject):
         sys.stderr.write("Failed to deregister node: %s" % spore_context.K_CONTEXT_NAME)
         raise
 
+    # deregister spore node
     try:
         mplugin.deregisterNode(spore_node.SporeNode.id)
     except:
         sys.stderr.write("Failed to deregister node: %s" % spore_node.SporeNode.name)
         raise
 
+    # delete menu
+    pm.deleteUI(MENU)
 
-def load_spore_template(node_name):
 
-    print 'ae cb'
-    #  try:
-    import AEsporeNodeTemplate
-    reload (AEsporeNodeTemplate)
-    from maya import mel
-    mel.eval('refreshEditorTemplates;')
-
-    ae_template = AEsporeNodeTemplate.AEsporeNodeTemplate(node_name)
-    print ae_template
-    #  except:
-    #      raise ImportWarning('Could not import sporeNode Attribute Editor ui')
+#  def load_spore_template(node_name):
+#
+#      print 'ae cb'
+#      #  try:
+#      import AEsporeNodeTemplate
+#      reload (AEsporeNodeTemplate)
+#      from maya import mel
+#      mel.eval('refreshEditorTemplates;')
+#
+#      ae_template = AEsporeNodeTemplate.AEsporeNodeTemplate(node_name)
+#      print ae_template
+#      #  except:
+#      #      raise ImportWarning('Could not import sporeNode Attribute Editor ui')
