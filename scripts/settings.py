ac00e7965a924232d6f760ddcd82b88c7728829d
anno <anno_schachner@gmx.at>
2018-06-23 10:01:30 +0200

    update settings and add to menu

--- scripts/settings.py
+++ scripts/settings.py
@@ -1,25 +1,21 @@
 import os
 import sys
 import json
-from logging import DEBUG, INFO, WARN, ERROR
+from logging import WARN
+
+from PySide2.QtCore import QObject, Slot
 
 import settings_ui
 import logging_util
 
-#  reload(settings_ui)
 
-try:
-    import requests
-    REPORT_DEFAULT = True
-except ImportError:
-    REPORT_DEFAULT = False
-
-class SporeGlobals(object):
+class SporeGlobals(QObject):
+    """ class manages spore's global preferences """
 
     default_prefs = {
                      'INITIAL_STARTUP': True, # Indicates that spore starts up for the first time
-                     'LOG_LEVEL': ERROR, # Set the log level for spore
-                     'AUTOMATIC_REPORT': REPORT_DEFAULT, # Submit reports automatically
+                     'LOG_LEVEL': WARN, # Set the log level for spore
+                     'AUTOMATIC_REPORT': False, # Submit reports automatically
                      'REPORT': True, # Enable/Disabel reporting
                      'SENDER': ' ' # Store sender email address
                      }
@@ -27,24 +23,27 @@ class SporeGlobals(object):
     def __init__(self):
 
         self.ui = settings_ui.SettingsUI()
+        self.ui.save_prefs.connect(self.dump_prefs)
+
         self.spore_globals = self.parse_prefs()
         self.logger = logging_util.SporeLogger(__name__, self['LOG_LEVEL'])
         self.fill_prefs_ui()
 
     def __getitem__(self, attr):
+
         return self.spore_globals[attr]
 
     def __setitem__(self, attr, val):
 
         if attr in self.spore_globals.keys():
-            if type(value) == type(self.spore_globals[pref]):
-                self.logger.debug('Set prefs: {}={}'.format(pref, value))
+            if type(val) == type(self.spore_globals[attr]):
+                self.logger.debug('Set prefs: {}={}'.format(attr, val))
                 self.spore_globals[attr] = val
                 self.dump_prefs()
             else:
-                raise TypeError('{} must be of type {}'.format(pref, type(self.spore_globals[pref])))
+                raise TypeError('{} must be of type {}'.format(attr, type(self.spore_globals[attr])))
         else:
-            raise KeyError('{} is not a valid option'.format(pref))
+            raise KeyError('{} is not a valid option'.format(attr))
 
     def __iter__(self):
         for attr, val in self.spore_globals.iteritems():
@@ -72,39 +71,46 @@ class SporeGlobals(object):
     def fill_prefs_ui(self):
         """ fill the settings ui with existing globals """
 
-        for key, val in self.spore_globals.iteritems():
-            print key, val
+        for key, val in self: #.spore_globals.iteritems():
             self.ui.add_pref_wdg(key, val)
 
-    def set_pref(self, pref, value):
-        """ set the given pref option to the given value.
-        :param pref: must match the a valid option
-        :type pref: string
-        :param value: type must match the required type
-        :type value: any """
+    #  def set_pref(self, pref, value):
+    #      """ set the given pref option to the given value.
+    #      :param pref: must match the a valid option
+    #      :type pref: string
+    #      :param value: type must match the required type
+    #      :type value: any """
+    #
+    #      if pref in self.spore_globals.keys():
+    #          if type(value) == type(self.spore_globals[pref]):
+    #              self.logger.warn('#DEPRECATED METHOD - USE __setitem__# Set prefs: {}={}'.format(pref, value))
+    #              self.spore_globals[pref] = value
+    #              self.dump_prefs()
+    #          else:
+    #              raise TypeError('{} must be of type {}'.format(pref, type(self.spore_globals[pref])))
+    #      else:
+    #          raise KeyError('{} is not a valid option'.format(pref))
+    #
+    #  def get_pref(self, pref):
+    #      """ get the value for the given pref option """
+    #
+    #      return self.spore_globals[pref]
 
-        if pref in self.spore_globals.keys():
-            if type(value) == type(self.spore_globals[pref]):
-                self.logger.debug('#DEPRECATED METHOD - USE __setitem__# Set prefs: {}={}'.format(pref, value))
-                self.spore_globals[pref] = value
-                self.dump_prefs()
-            else:
-                raise TypeError('{} must be of type {}'.format(pref, type(self.spore_globals[pref])))
-        else:
-            raise KeyError('{} is not a valid option'.format(pref))
-
-    def get_pref(self, pref):
-        """ get the value for the given pref option """
-
-        return self.spore_globals[pref]
-
-    def dump_prefs(self):
-        """ dump the current setting to file """
+    @Slot(dict)
+    def dump_prefs(self, prefs=None):
+        """ dump the current setting to file.
+        if a prefs argument is given dump the given settings instead. """
 
         pref_file = os.path.join(os.environ['SPORE_PREFS_DIR'], 'spore_prefs.json')
         with open(pref_file, 'w') as f:
-            json.dump(self.default_prefs, f)
+            if prefs:
+                self.spore_globals = prefs
+
+            json.dump(self.spore_globals, f)
+            self.logger.debug('Spore globals dumped: {}'.format(self.spore_globals))
+
+        self.logger.set_log_level(self.spore_globals['LOG_LEVEL'])
 
     def show(self):
-        self.ui.show()
+        self.ui.show(dockable=True)
 
--- scripts/ui/settings_ui.py
+++ scripts/ui/settings_ui.py
@@ -1,26 +1,34 @@
+import sys
 
 from PySide2.QtWidgets import (QWidget, QGridLayout, QPushButton, QFrame,
                                QHBoxLayout, QVBoxLayout, QLabel, QScrollArea,
                                QSpacerItem, QLineEdit, QAction, QMenu,
-                               QComboBox, QSpinBox, QDoubleSpinBox)
+                               QComboBox, QSpinBox, QDoubleSpinBox, QCheckBox)
 from PySide2.QtWidgets import QSizePolicy
 from PySide2.QtCore import Signal, QObject, Qt, QEvent
 from PySide2.QtGui import QPalette, QColor, QPixmap, QIcon
 
+from maya import cmds
 from maya.app.general.mayaMixin import MayaQWidgetDockableMixin
 
-from maya import cmds
+import logging_util
 
 
 class SettingsUI(MayaQWidgetDockableMixin, QWidget):
 
+    save_prefs = Signal(dict)
+
     def __init__(self, parent=None):
         super(SettingsUI, self).__init__(parent=parent)
+
+        #  log_lvl = sys._global_spore_dispatcher.spore_globals['LOG_LEVEL']
+        #  self.logger = logging_util.SporeLogger(__name__, log_lvl)
         self.build_ui()
+        self.pref_tracking_dir = {}
 
     def build_ui(self):
         self.setWindowTitle('Spore Preferences')
-        self.setGeometry(50, 50, 400, 550)
+        self.setGeometry(250, 250, 400, 150)
 
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
@@ -29,24 +37,55 @@ class SettingsUI(MayaQWidgetDockableMixin, QWidget):
         self.save_btn = QPushButton('Save')
         self.layout.addWidget(self.save_btn)
 
+        self.save_btn.clicked.connect(self.about_to_save)
+
     def add_pref_wdg(self, name, value):
 
         if isinstance(value, bool):
             wdg = BoolWidget(name, value)
             self.layout.insertWidget(0, wdg)
+            self.pref_tracking_dir[wdg.name_lbl] = wdg.bool_cbx
         elif isinstance(value, str) or isinstance(value, unicode):
             wdg = StringWidget(name, value)
             self.layout.insertWidget(0, wdg)
+            self.pref_tracking_dir[wdg.name_lbl] = wdg.line_edt
         elif isinstance(value, int):
             wdg = IntegerWidget(name, value)
             self.layout.insertWidget(0, wdg)
+            self.pref_tracking_dir[wdg.name_lbl] = wdg.int_spn
         elif isinstance(value, float):
             wdg = FloatWidget(name, value)
             self.layout.insertWidget(0, wdg)
+            self.pref_tracking_dir[wdg.name_lbl] = wdg.float_spn
         else:
             print 'no assignment for', name, value, type(value)
             pass
 
+    def about_to_save(self):
+        """ triggered by the save button.
+        emit the save_prefs signal and send the resulting settings """
+
+        #  self.logger.debug('About to emit save settings.')
+        prefs = {}
+        for attr_lbl, val_wdg in self.pref_tracking_dir.iteritems():
+            attr = attr_lbl.text()
+
+            val = None
+            if isinstance(val_wdg, QCheckBox):
+                val = val_wdg.isChecked()
+            elif isinstance(val_wdg, QLineEdit):
+                val = val_wdg.text()
+            elif isinstance(val_wdg, QSpinBox)\
+            or isinstance(val_wdg, QDoubleSpinBox):
+                val = val_wdg.value()
+            else:
+                #  self.logger.error('Unknown widget type: {}'.format(val_wdg))
+                raise RuntimeError('Impossible')
+
+            prefs[attr] = val
+
+        self.save_prefs.emit(prefs)
+        self.close()
 
 class BoolWidget(QWidget):
     def __init__(self, name, value):
@@ -60,10 +99,8 @@ class BoolWidget(QWidget):
         self.name_lbl.setFixedWidth(150)
         layout.addWidget(self.name_lbl)
 
-        self.bool_cbx = QComboBox()
-        self.bool_cbx.addItem('True')
-        self.bool_cbx.addItem('False')
-        self.bool_cbx.setCurrentIndex(1 if value else 0)
+        self.bool_cbx = QCheckBox()
+        self.bool_cbx.setChecked(value)
         layout.addWidget(self.bool_cbx)
 
 
@@ -99,9 +136,9 @@ class IntegerWidget(QWidget):
         self.name_lbl.setFixedWidth(150)
         layout.addWidget(self.name_lbl)
 
-        self.inst_spn = QSpinBox()
-        self.inst_spn.setValue(value)
-        layout.addWidget(self.inst_spn)
+        self.int_spn = QSpinBox()
+        self.int_spn.setValue(value)
+        layout.addWidget(self.int_spn)
 
 class FloatWidget(QWidget):
     def __init__(self, name, value):
@@ -116,9 +153,9 @@ class FloatWidget(QWidget):
         self.name_lbl.setFixedWidth(150)
         layout.addWidget(self.name_lbl)
 
-        self.inst_spn = QDoubleSpinBox()
-        self.inst_spn.setValue(value)
-        layout.addWidget(self.inst_spn)
+        self.float_spn = QDoubleSpinBox()
+        self.float_spn.setValue(value)
+        layout.addWidget(self.float_spn)
 
 
 
